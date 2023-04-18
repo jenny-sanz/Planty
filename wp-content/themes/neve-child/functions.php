c7c0243cad2678112699ec848caec050739a7904
jennifer sanz <jennysanz02@gmail.com>
2023-04-18 20:05:34 +0200

    modif fichier function chargement css

--- wp-content/themes/neve-child/functions.php
+++ wp-content/themes/neve-child/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function neve_child_load_css() {
-	wp_enqueue_style( 'neve-child-style', get_stylesheet_uri(), array( 'neve-style' ), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'neve-child-style', get_stylesheet_uri(), array( 'neve-style' ), filemtime(get_stylesheet_directory()) . '/style.css');
 }
 add_action( 'wp_enqueue_scripts', 'neve_child_load_css' );
 
