9a9b9c72a6757c923f6a442e3ee4bfad490af542
jennifer sanz <jennysanz02@gmail.com>
2023-04-18 17:42:23 +0200

    création footer personnalisé

---
+++ wp-content/themes/neve-child/footer.php
@@ -0,0 +1,18 @@
+<?php
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+?>
+<!-- coder mon footer  -->
+<footer class="mon-footer">
+    <p>
+        <a href="">Mentions légales</a>
+    </p>
+</footer>
+<?php wp_footer(); ?>
+
+</body>
+</html>
